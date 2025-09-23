46f32d72c603e7c188b1773c4ae203daf93ecbda
piyush rawat <rawatpiyush006@gmail.com>
2025-09-23 13:59:35 +0530

    firebase and env

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCIUb38ebiQ9UtgV2b8zlvuUKx_SBmJpcc",
+  apiKey: import.meta.env.VIET_FIREBASE_APIKEY,
   authDomain: "taskflow-ba113.firebaseapp.com",
   projectId: "taskflow-ba113",
   storageBucket: "taskflow-ba113.firebasestorage.app",
