c6eca9166aee44a39312d04ae54eda0740069834
piyush rawat <rawatpiyush006@gmail.com>
2025-09-23 16:34:54 +0530

    add

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,19 @@
-// Import the functions you need from the SDKs you need
+// firebase.js
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VIET_FIREBASE_APIKEY,
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,   // typo fixed: VITE_ not VIET_
   authDomain: "taskflow-ba113.firebaseapp.com",
   projectId: "taskflow-ba113",
-  storageBucket: "taskflow-ba113.firebasestorage.app",
+  storageBucket: "taskflow-ba113.appspot.com",   // fix: remove `.firebasestorage.app`
   messagingSenderId: "580676384596",
   appId: "1:580676384596:web:7387e324cbfaa851edbac1",
   measurementId: "G-03BNKSRCN9"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// ✅ export these for your Login/SignUp
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
